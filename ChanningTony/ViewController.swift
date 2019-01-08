aeec5c32051bca15941b081285bbc31be1d51b82
Anthony DiPadova <tony.19@dartmouth.edu>
2019-01-08 17:39:33 -0500

    Added blue floating capsule to near the world origin

--- ChanningTony/ViewController.swift
+++ ChanningTony/ViewController.swift
@@ -19,5 +19,15 @@ class ViewController: UIViewController {
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         sceneView.session.run(config)
+        
+        // create capsule
+        let capsuleNode = SCNNode(geometry: SCNCapsule(capRadius: 0.03, height: 0.1))
+        capsuleNode.position = SCNVector3(0.1, 0.1, -0.1) // position relative to world origin
+        
+        // add texture
+        capsuleNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue //1
+        capsuleNode.eulerAngles = SCNVector3(0,0,Double.pi/2)//2
+        
+        sceneView.scene.rootNode.addChildNode(capsuleNode) // add capsule to view
     }
 }
